109fd7ba5519da72b2040ec7a297259f3e8be6bf
tornadoravi <tornadoravi1453@gmail.com>
2022-09-06 09:55:12 +0530

    update

--- tennisTournamentschedule.js
+++ tennisTournamentschedule.js
@@ -1,86 +1,114 @@
 tornament = {
     tennis: [
-        { playerId: "TOR2221", playerRank: 1, playerName: "name1", noOfMatchs: 222, playerCountry: "ind" },
-        { playerId: "TOR2222", playerRank: 2, playerName: "name2", noOfMatchs: 256, playerCountry: "us" },
-        { playerId: "TOR2223", playerRank: 3, playerName: "name3", noOfMatchs: 266, playerCountry: "uk" },
-        { playerId: "TOR2224", playerRank: 4, playerName: "name4", noOfMatchs: 286, playerCountry: "ger" },
-        { playerId: "TOR2225", playerRank: 5, playerName: "name5", noOfMatchs: 167, playerCountry: "ity" },
-        { playerId: "TOR2226", playerRank: 6, playerName: "name6", noOfMatchs: 287, playerCountry: "ukr" },
-        { playerId: "TOR2227", playerRank: 7, playerName: "name7", noOfMatchs: 299, playerCountry: "rus" },
-        { playerId: "TOR2228", playerRank: 8, playerName: "name8", noOfMatchs: 272, playerCountry: "afr" }
+        { playerId: "TOR2221", playerRank: 8, playerName: "name1", noOfMatchs: 222, playerCountry: "ind" },
+        { playerId: "TOR2222", playerRank: 7, playerName: "name2", noOfMatchs: 256, playerCountry: "us" },
+        { playerId: "TOR2223", playerRank: 6, playerName: "name3", noOfMatchs: 266, playerCountry: "uk" },
+        { playerId: "TOR2224", playerRank: 5, playerName: "name4", noOfMatchs: 286, playerCountry: "ger" },
+        { playerId: "TOR2225", playerRank: 4, playerName: "name5", noOfMatchs: 167, playerCountry: "ity" },
+        { playerId: "TOR2226", playerRank: 3, playerName: "name6", noOfMatchs: 287, playerCountry: "ukr" },
+        { playerId: "TOR2227", playerRank: 2, playerName: "name7", noOfMatchs: 299, playerCountry: "rus" },
+        { playerId: "TOR2228", playerRank: 1, playerName: "name8", noOfMatchs: 272, playerCountry: "afr" }
     ],
     startdate: ["2022/09/03"]
 }
-let accessingObject = (tornament.tennis);
-let accesingdate = tornament.startdate
-// 1st rount versus________________________________________________________________
-matchtype = "------1st round-------"
-x = 1
-function schulte(element, matchstype, days) {
-    schultteling = []
-    schultteling.push(matchstype)
-    for (let i = 1; i <= element.length / 2; i++) {
+intputdate = tornament.startdate
+let accessElement = tornament.tennis
+function dynamicsort(property) {
+    return function (a, b) {
 
-        date = new Date(days)
-        next = new Date(date.getTime() + (x) * 24 * 60 * 60 * 1000)
+        if (a[property] < b[property]) {
+            return -1
+        } else if (a[property] > b[property]) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+}
+
+rankbaseShortList = ((accessElement).sort(dynamicsort("playerRank")))
+matchlevel = "-----*****matchs*****-----"
+//  schedule-------------------------
+function schedule(sinElement, matchType, dayx) {
+    scheduling = []
+    scheduling.push(matchType)
+    match = 1
+    x = 1
+    date = new Date(dayx)
+    s = 6 - (date.getDay())
+    for (let i = 1; i <= sinElement.length / 2; i++) {
+        next = new Date(date.getTime() + (s + x) * 24 * 60 * 60 * 1000)
         tomorrow = String(next).slice(0, 15);
-        day = tomorrow
-        a = element.length - i
-        const match =
-        {
+        const versusTeam = {
+            Matchno: match,
             matchDate: tomorrow,
-            challengerPlayerName: (element[i - 1]).playerName,
-            challengerPlayerid: (element[i - 1]).playerId,
-            challengermatchplayed: (element[i - 1]).noOfMatchs + 1,
-            opposerPlayerName: (element[a]).playerName,
-            opposerPlayerid: (element[a]).playerId,
-            opposermatchplayed: (element[a]).noOfMatchs + 1
-        }
-        schultteling.push(match)
-        x += 1
-    }
-    console.log(schultteling)
-    winloss(schultteling)
-}
-// win or los________________________________________________________________
-winorlose = []
-function prop() {
-    upcome = ['0', "win", "lose"]
-    x = Math.floor((Math.random() * 2) + 1)
-    return upcome[x]
-}
-function winloss(list) {
-    winningresult = []
-    resultOfMatch = []
-    for (i = 1; i <= list.length; i++) {
-        winningresult[i] = []
-        winningresult[i].push(prop())
-        if (winningresult[i - 1] == "win") {
-            const winner = {
-                playerName: list[i - 1].challengerPlayerName,
-                playerId: list[i - 1].challengerPlayerid,
-                noOfMatchs: list[i - 1].challengermatchplayed
-            }
-            resultOfMatch.push(winner)
-        }
-        else if (winningresult[i - 1] == "lose") {
+            player1ID: sinElement[i - 1].playerId,
+            player1NAME: sinElement[i - 1].playerName,
+            player1country: sinElement[i - 1].playerCountry,
+            player1matchs: sinElement[i - 1].noOfMatchs + 1,
 
-            const winner = {
-                playerName: list[i - 1].opposerPlayerName,
-                playerId: list[i - 1].opposerPlayerid,
-                noOfMatchs: list[i - 1].opposermatchplayed
+            player2ID: (sinElement[sinElement.length - i]).playerId,
+            player2NAME: (sinElement[sinElement.length - i]).playerName,
+            player2country: (sinElement[sinElement.length - i]).playerCountry,
+            player2matchs: (sinElement[sinElement.length - i]).noOfMatchs + 1
+        }
+        x += 1
+        match++
+        scheduling.push(versusTeam)
+    } console.log(scheduling)
+    matchdatez = tomorrow
+    date = new Date(matchdatez)
+    s = 6 - (date.getDay())
+
+    next = new Date(date.getTime() + (s) * 24 * 60 * 60 * 1000)
+    nextdayz = String(next).slice(0, 15);
+
+    winorlose(scheduling)
+
+}
+// random __________________________
+function randoms() {
+    Results = ["0", "winner", "loser"]
+    matchResults = Math.floor(Math.random() * 2) + 1
+    // console.log(Results[matchResults])
+    return Results[matchResults]
+}
+
+function winorlose(winningplayer) {
+
+    perResult = []
+    playerresults = []
+    for (i = 1; i <= winningplayer.length - 1; i++) {
+        playerresults[i] = []
+        playerresults[i].push(randoms())
+        if (playerresults[i] == "winner") {
+            const matchsresultsobj = {
+                Results: i,
+                playerId: winningplayer[i].player1ID,
+                playerName: winningplayer[i].player1NAME,
+                playerCountry: winningplayer[i].player1country,
+                noOfMatchs: winningplayer[i].player1matchs
             }
-            resultOfMatch.push(winner)
+            perResult.push(matchsresultsobj)
+
+        }
+        else if (playerresults[i] == "loser") {
+            const matchsresultsobj = {
+                Results: i,
+                playerId: winningplayer[i].player1ID,
+                playerName: winningplayer[i].player2NAME,
+                playerCountry: winningplayer[i].player2country,
+                noOfMatchs: winningplayer[i].player2matchs
+            }
+            perResult.push(matchsresultsobj)
+
         }
     }
-    console.log("-----------winner list------------");
-    len = schultteling.length
-    next = new Date(date.getTime() + (len) * 24 * 60 * 60 * 1000)
-    tomorrows = String(next).slice(0, 15);
-    console.log(resultOfMatch)
-    if (resultOfMatch.length > 1) {
-        matchtype = "----------next rount-----------"
-        schulte(resultOfMatch, matchtype, tomorrows)
+    console.log("*****winner results*****")
+    console.table(perResult)
+    if (perResult.length > 1) {
+        matchType = "semifinal"
+        schedule(perResult, matchlevel, nextdayz)
     }
 }
-schulte(accessingObject, matchtype, accesingdate)
+schedule(rankbaseShortList, matchlevel, intputdate)
